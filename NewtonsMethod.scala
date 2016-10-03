f5f44d743b2771a4f5c57e8f408288c6f88ba8e6
jaken herman <jakenherman7@gmail.com>
2016-10-03 15:46:31 -0500

    Horner's Added

--- NewtonsMethod.scala
+++ NewtonsMethod.scala
@@ -29,8 +29,10 @@ object NewtonsMethod {
       val guess = 0.9+i
       // pass f(x) and f'(x) to the Newton's Method function, along with the initial guess
       // and tolerance 
+      print("Iterations for guess " + guess + " is : ")
       val answer = newtonsMethod(fx, fxPrime, guess, tolerance)
-      println("The answer for guess " + guess + " is : " + answer)
+      println("The answer for guess " + guess + " is : " + "%.4f".format(answer))
+      println()
     }
 
   }
@@ -41,12 +43,13 @@ object NewtonsMethod {
                     tolerance: Double): Double = {
     var x1 = x
     var xNext = nmh(fx, fxPrime, x1)
+    var count = 0
     while (Math.abs(xNext - x1) > tolerance) {
       x1 = xNext
-      println(xNext)
       xNext = nmh(fx, fxPrime, x1)
+      count = count + 1
     }
-    
+    println(count)
     return xNext
   }
 
@@ -57,6 +60,17 @@ object NewtonsMethod {
                           fxPrime: Double => Double,
                           x: Double): Double = {
     return x - fx(x) / fxPrime(x)
+  }
+
+  def horner(coeffs:List[Double], x:Double)=
+   coeffs.reverse.foldLeft(0.0){(a,c)=> a*x+c}
+ 
+
+ // val coeffs=List(1.0, -28.0, 322.0, -1960.0, 6769.0, -13132.0, 13068.0, -5040.0)
+  val coeffs=List(-5040.0, 13068.0, -13132.0, 6769.0, -1960.0, 322.0, -28.0, 1.0)
+  for(i <- 1 to 10){
+  println("Evaluation using Horner's (x = " + i + ") : " + horner(coeffs, i))
+  println()
   }
 
 }
